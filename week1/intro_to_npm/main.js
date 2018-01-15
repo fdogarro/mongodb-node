eb432aaa8c86199ee18b5d8b9e32f648e1aba251
fdw101 <felicia.ogarro@gmail.com>
2018-01-15 11:42:40 -0500

    week 1 homework completed

--- week1/intro_to_npm/main.js
+++
@@ -1,3 +0,0 @@
-const express = require('express'); 
-const cons = require('consolidate'); 
-const mongodb = require('mongodb'); 
--- week1/node-driver/app.js
+++ week1/node-driver/app.js
@@ -1,25 +1,30 @@
+const express = require('express'); 
+const app = express(); 
+const engines = require('consolidate'); 
 const { MongoClient } = require('mongodb'); 
 const assert = require('assert');
-
 const url = 'mongodb://localhost:27017/video';
+
+app.engine('html', engines.nunjucks); 
+app.set('view engine', 'html'); 
+app.set('views', __dirname + '/views'); 
 
 MongoClient.connect(url, (err, db) => {
 
     assert.equal(null, err);
     console.log("Successfully connected to server");
 
-    // Find some documents in our collection
-    db.collection('movies').find({}).toArray((err, docs) => {
+    app.get('/', (req, res) => {
+	    db.collection('movies').find({}).toArray((err, docs) => {
+	    		res.render('movies', { 'movies': docs });
+	    });
+    }); 
 
-        // Print the documents returned
-        docs.forEach((doc) => {
-            console.log(doc.title);
-        });
+    app.use((req, res) => {
+    	res.sendStatus(404); 
+    }); 
 
-        // Close the DB
-        db.close();
-    });
-
-    // Declare success
-    console.log("Called find()");
+    const server = app.listen(3000, () => {
+    	const port = server.address().port; 
+    }); 
 });
